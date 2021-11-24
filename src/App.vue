1ff69216b47877facb30806bdc15641853207d96
EstebanEF <eef251195@gmail.com>
2021-11-23 20:12:52 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,12 @@
     <router-link class="three columns button button-primary" 
       to="/">Home</router-link>
     <router-link class="three columns button button-primary" 
-      to="/book">Books</router-link>
-    <router-link class="three columns button button-primary" 
-      to="/author">Authors</router-link>
-    <router-link class="three columns button button-primary" 
-      to="/publisher">Publishers</router-link>
+      to="/book">Articulos Cientificos</router-link>
   </div>
  <router-view />
   <div class="row">
     <button disabled="disabled" class="twelve columns button-primary">
-      Copyright (c) 2020 - Armando Arce-Orozco
+      Copyright (c) 2021 - Esteban Espinoza Fallas & Marco Murillo Sanchez
     </button>
   </div>
 </div>
