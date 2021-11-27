4843792dc301cff577074697ed4a4b24b559a451
EstebanEF <eef251195@gmail.com>
2021-11-27 12:49:46 -0600

    cambios en el html

--- src/components/PaperDetails.vue
+++ src/components/PaperDetails.vue
@@ -1,109 +1,123 @@
 <!-- BookDetails.vue -->
 <template>
   <div class="row">
-   <div class="eleven column" style="margin-top: 5%">
-    <h2>{{title}}</h2>
-     <form>
-     <div class="row">
-      <div class="six columns">
-       <label for="titleInput">Title</label>
-       <input class="u-full-width" type="text"
-         v-model="book.title">
-      </div>
-      <div class="six columns">
-       <label for="authorInput">Author</label>
-       <input class="u-full-width" type="text"
-          v-model="book.author">
-      </div>
-     </div>
-     <div class="row">
-      <div class="six columns">
-       <label for="publisherInput">Publisher</label>
-       <input class="u-full-width" type="text"
-          v-model="book.publisher">
-      </div>
-      <div class="six columns">
-       <label for="editionInput">Edition</label>
-       <input class="u-full-width" type="text"
-         v-model="book.edition">
-      </div>
-     </div>
-     <div class="row">
-      <div class="six columns">
-       <label for="copyrightInput">Copyright</label>
-       <input class="u-full-width" type="number"
-          v-model="book.copyright">
-      </div>
-      <div class="six columns">
-       <label for="languageInput">Language</label>
-       <input class="u-full-width" type="text"
-         v-model="book.language">
-      </div>
-     </div>
-     <div class="row">
-      <router-link class="button button-primary" 
-        to="/book">Back</router-link>
-       <a v-if='edit' class="button button-primary" style="float: right"
-         v-on:click="updateBook(book._id)">Update</a>
-       <a v-if='create' class="button button-primary" style="float: right"
-         v-on:click="createBook()">Create</a>
-     </div>
-    </form>
+    <div class="eleven column" style="margin-top: 5%">
+      <h2>{{ title }}</h2>
+      <form>
+        <div class="row">
+          <div class="six columns">
+            <label for="titleInput">Title</label>
+            <input class="u-full-width" type="text" v-model="paper.title" />
+          </div>
+          <div class="six columns">
+            <label for="authorInput">Author</label>
+            <input class="u-full-width" type="text" v-model="paper.author" />
+          </div>
+        </div>
+        <div class="row">
+          <div class="six columns">
+            <label for="editionInput">Pages</label>
+            <input class="u-full-width" type="text" v-model="paper.pages" />
+          </div>
+          <div class="six columns">
+            <label for="copyrightInput">Copyright</label>
+            <input
+              class="u-full-width"
+              type="number"
+              v-model="paper.copyright"
+            />
+          </div>
+        </div>
+        <div class="row">
+          <div class="six columns">
+            <label for="languageInput">Language</label>
+            <input class="u-full-width" type="text" v-model="paper.language" />
+          </div>
+          <div class="six columns">
+            <label for="languageInput">URL</label>
+            <input class="u-full-width" type="text" v-model="paper.url" />
+          </div>
+        </div>
+        <div class="row">
+          <router-link class="button button-primary" to="/book"
+            >Back</router-link
+          >
+          <a
+            v-if="edit"
+            class="button button-primary"
+            style="float: right"
+            v-on:click="updateBook(paper._id)"
+            >Update</a
+          >
+          <a
+            v-if="create"
+            class="button button-primary"
+            style="float: right"
+            v-on:click="createBook()"
+            >Create</a
+          >
+        </div>
+      </form>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import { useRoute } from 'vue-router'
+import { useRoute } from "vue-router";
 
 export default {
   name: "Detalle Acticuloss cientificos",
-  props: ['create','edit','create'],
+  props: ["create", "edit", "create"],
   data() {
     return {
       title: "Datos del Articulo",
-      papers: {}
-    }
+      papers: {},
+    };
   },
   mounted() {
-    const route = useRoute()
-    if (route.params.id != null)
-      this.findPaper(route.params.id);
+    const route = useRoute();
+    if (route.params.id != null) this.findPaper(route.params.id);
     else {
       this.paper = {
-        '_id': Math.floor(Math.random()*100000000),'title':'','edition':'',
-        'copyright':0,'language':'','pages':0,'author':'', 'url':""};
+        _id: Math.floor(Math.random() * 100000000),
+        title: "",
+        author: "",
+        copyright: 0,
+        language: "",
+        pages: 0,
+        author: "",
+        url: "",
+      };
     }
   },
   methods: {
-    findPaper: function(id) {
-      fetch(this.url+'/.netlify/functions/paperFind/'+id,
-      { headers: {'Accept': 'application/json'}})
-      .then((response) => response.json())
-      .then((items) => {
-       this.paper = items[0];
+    findPaper: function (id) {
+      fetch(this.url + "/.netlify/functions/paperFind/" + id, {
+        headers: { Accept: "application/json" },
       })
+        .then((response) => response.json())
+        .then((items) => {
+          this.paper = items[0];
+        });
     },
-    updatePaper: function(id) {
-      fetch(this.url+'/.netlify/functions/paperUpdate/'+id,
-        { headers: {'Content-Type':'application/json'},
-          method: 'PUT',
-          body: JSON.stringify(this.paper)})
-        .then((data) => {
-          this.$router.push('/paper');
-        }
-      )
+    updatePaper: function (id) {
+      fetch(this.url + "/.netlify/functions/paperUpdate/" + id, {
+        headers: { "Content-Type": "application/json" },
+        method: "PUT",
+        body: JSON.stringify(this.paper),
+      }).then((data) => {
+        this.$router.push("/paper");
+      });
     },
-    createPaper: function() {
-      fetch(this.url+'/.netlify/functions/paperInsert',
-        { headers: {'Content-Type':'application/json'},
-          method: 'POST',
-          body: JSON.stringify(this.paper)})
-        .then((data) => {
-           this.$router.push('/paper');
-        }
-      )
-    }
-  }
+    createPaper: function () {
+      fetch(this.url + "/.netlify/functions/paperInsert", {
+        headers: { "Content-Type": "application/json" },
+        method: "POST",
+        body: JSON.stringify(this.paper),
+      }).then((data) => {
+        this.$router.push("/paper");
+      });
+    },
+  },
 };
 </script>
--- src/components/PaperIndex.vue
+++ src/components/PaperIndex.vue
@@ -1,71 +1,75 @@
 <!-- BookIndex.vue -->
 <template>
   <div class="row">
-   <div style="margin-top: 5%">
-     <h2>{{title}}</h2>
-     <table><thead>
-       <tr>
-         <th>Title</th>
-        <th>Autor</th>
-        <th>Publisher</th>
-        <th>Edition</th>
-        <th class="text-center">Actions</th>
-	   </tr>
-       </thead><tbody>
-       <tr v-for='book in books'>
-       <td>{{book.title}}</td>
-       <td>{{book.author}}</td>
-       <td>{{book.publisher}}</td>
-       <td>{{book.edition}}</td>
-       <td>
-       <router-link class="button"
-         :to="'/book/show/'+book._id">Show</router-link>
-       &nbsp;
-       <router-link class="button"
-         :to="'/book/edit/'+book._id">Edit</router-link>
-       &nbsp;
-       <a class="button"
-         v-on:click="deleteBook(book._id)">Erase</a>
-       </td>
-       </tr></tbody>
-     </table>
-     <router-link class="button button-primary" 
-       to="/book/create">New</router-link>
-   </div>
+    <div style="margin-top: 5%">
+      <h2>{{ title }}</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Autor</th>
+            <th>Paginas</th>
+            <th>Copyright</th>
+            <th class="text-center">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="paper in papers">
+            <td>{{ paper.title }}</td>
+            <td>{{ paper.author }}</td>
+            <td>{{ paper.pages }}</td>
+            <td>{{ paper.copyright }}</td>
+            <td>
+              <router-link class="button" :to="'/papers/show/' +paper._id"
+                >Show</router-link
+              >
+              &nbsp;
+              <router-link class="button" :to="'/papers/edit/' + paper._id"
+                >Edit</router-link
+              >
+              &nbsp;
+              <a class="button" v-on:click="deleteBook(paper._id)">Erase</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <router-link class="button button-primary" to="/paper/create"
+        >New</router-link
+      >
+    </div>
   </div>
 </template>
 
 <script>
-	
 export default {
-  name: "Book Index",
+  name: "Paper Index",
   data() {
     return {
-      title: 'Book List',
-      books: []
+      title: "Paper List",
+      books: [],
     };
   },
   mounted() {
-    this.allBooks()
+    this.allBooks();
   },
   methods: {
     allBooks() {
-      fetch(this.url+'/.netlify/functions/bookFindAll',
-        { headers: {'Accept': 'application/json'}})
+      fetch(this.url + "/.netlify/functions/bookFindAll", {
+        headers: { Accept: "application/json" },
+      })
         .then((response) => response.json())
         .then((items) => {
           this.books = items;
-        })
-     },
-     deleteBook(id) {
-       fetch(this.url+'/.netlify/functions/bookDelete/'+id,
-         { headers: {'Content-Type': 'application/json'},
-           method: 'DELETE'})
-          .then((items) => {
-            this.allBooks();
-          }
-        )
-     }
-  }
+        });
+    },
+    deleteBook(id) {
+      fetch(this.url + "/.netlify/functions/bookDelete/" + id, {
+        headers: { "Content-Type": "application/json" },
+        method: "DELETE",
+      }).then((items) => {
+        this.allBooks();
+      });
+    },
+  },
 };
 </script>
