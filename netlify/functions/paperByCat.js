f76b60d390b8f42c313cf9e02aa6650aafa8eb79
mark532 <mmurillo532@gmail.com>
2021-11-27 21:49:21 -0600

    commit

---
+++ netlify/functions/paperByCat.js
@@ -0,0 +1,24 @@
+"use strict"
+
+const mongoPromise = require('./mongoDB');
+
+const headers = require('./headersCORS');
+
+exports.handler = async(event, context) => {
+
+    if (event.httpMethod == "OPTIONS") {
+        return { statusCode: 200, headers, body: "OK" };
+    }
+
+    try {
+        const client = await mongoPromise;
+        const id = event.path.split("/").reverse()[0];
+        const papers =
+            await client.db("articles").collection("papers").find({ categoria: id }).toArray();
+
+        return { statusCode: 200, headers, body: JSON.stringify(papers) };
+    } catch (error) {
+        console.log(error);
+        return { statusCode: 400, headers, body: JSON.stringify(error) };
+    }
+};
