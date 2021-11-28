9c194dfb3077aae94e6b7ca98db52f4dc695379e
EstebanEF <eef251195@gmail.com>
2021-11-27 21:37:21 -0600

    Update PaperIndex.vue

--- src/components/PaperIndex.vue
+++ src/components/PaperIndex.vue
@@ -3,6 +3,35 @@
   <div class="row">
     <div style="margin-top: 5%">
       <h2>{{ title }}</h2>
+      <button class="accordion">Section 1</button>
+      <div class="panel">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+
+      <button class="accordion">Section 2</button>
+      <div class="panel">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+
+      <button class="accordion">Section 3</button>
+      <div class="panel">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
       <table>
         <thead>
           <tr>
@@ -28,7 +57,9 @@
                 >Editar</router-link
               >
               &nbsp;
-              <a class="button" v-on:click="deletePaper(paper._id)">Eliminar </a>
+              <a class="button" v-on:click="deletePaper(paper._id)"
+                >Eliminar
+              </a>
             </td>
           </tr>
         </tbody>
@@ -51,6 +82,20 @@ export default {
   },
   mounted() {
     this.allPapers();
+    let acc = document.getElementsByClassName("accordion");
+    let i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
   },
   methods: {
     allPapers() {
