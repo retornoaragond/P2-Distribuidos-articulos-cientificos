08035c0d5a5a926c08d1f9433d2305b3e42ef3e9
mark532 <mmurillo532@gmail.com>
2021-11-27 21:25:06 -0600

    E.categorias

---
+++ netlify/functions/paperDistinct.js
@@ -0,0 +1,24 @@
+"use strict"
+
+const mongoPromise = require('./mongoDB');
+
+const headers = require('./headersCORS');
+
+exports.handler = async(event, context) => {
+
+    if (event.httpMethod == "OPTIONS") {
+        return { statusCode: 200, headers, body: "OK" };
+    }
+
+    try {
+        const client = await mongoPromise;
+        const id = parseInt(event.path.split("/").reverse()[0]);
+        const papers =
+            await client.db("articles").collection("papers").distinct('categoria');
+
+        return { statusCode: 200, headers, body: JSON.stringify(papers) };
+    } catch (error) {
+        console.log(error);
+        return { statusCode: 400, headers, body: JSON.stringify(error) };
+    }
+};
